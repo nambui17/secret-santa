85e0c9d5ebed159bdfd92fea509e5e937352f397
nambui <nam.bui17@icloud.com>
2023-08-17 15:06:23 -0500

    Initial commit

---
+++ secret-santa/secret_santaApp.swift
@@ -0,0 +1,17 @@
+//
+//  secret_santaApp.swift
+//  secret-santa
+//
+//  Created by Nam Bui on 8/17/23.
+//
+
+import SwiftUI
+
+@main
+struct secret_santaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ secret-santaUITests/secret_santaUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  secret_santaUITestsLaunchTests.swift
+//  secret-santaUITests
+//
+//  Created by Nam Bui on 8/17/23.
+//
+
+import XCTest
+
+final class secret_santaUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
